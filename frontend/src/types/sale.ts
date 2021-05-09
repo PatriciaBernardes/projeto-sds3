eb9bb58d636b942d2058f9caa4a63fdd8c41f0c4
Patrícia Bernardes Barcelos <barcelosbernardespatricia@gmail.com>
2021-05-09 09:51:22 -0300

    DataTable integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,3 +1,26 @@
+import { Seller } from "./seller"
+
+export type Sales = {    
+    id: number;
+    visited: number;
+    deals: number;
+    amount: number;
+    date: string;
+    seller: Seller;
+}
+
+export type SalePage = {
+   content?: Sales[];
+   last: boolean;
+   totalElements: number;
+   totalPages: number;
+   size?: number;
+   number: number;
+   first: boolean;
+   numberOfElements?: number;
+   empty?: boolean;
+}
+
 export type SaleSum = {
     sellerName: string;
     sum: number;
